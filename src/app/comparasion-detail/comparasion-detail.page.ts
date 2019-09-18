e13d789e12b6ffe5b170a2e20b45b8ec78175696
chen <ruochenhou@gmail.com>
2019-09-18 22:18:50 +1000

    update

--- src/app/comparasion-detail/comparasion-detail.page.ts
+++ src/app/comparasion-detail/comparasion-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-comparasion-detail',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComparasionDetailPage implements OnInit {
 
-  constructor() { }
+
+  myProfile:any;
+  mateProfile:any;
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
-  }
+    // 把localstorage 中的内容读取，与myProfile 比较
+    var zhangsan: any = new Object();
+    zhangsan.name = " zhangsan ";
+    zhangsan.dob = " 01/01/1982";
+    zhangsan.gender = "male";
+    zhangsan.swmming = true;
+    zhangsan.baskball = true;
 
+    var xiaohong: any = new Object();
+    xiaohong.name = "xiaohong";
+    xiaohong.dob = "01/01/1981";
+    xiaohong.gender = "Female";
+    xiaohong.swmming = true;
+    xiaohong.baskball = true;
+
+
+    this.myProfile = zhangsan;
+    this.mateProfile = xiaohong;
+
+  }
+  recommendationList(){
+    this.navCtrl.navigateForward("/recommendation");
+
+  }
 }
